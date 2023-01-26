5e0d73e0f7471e066641eb5a11f3cd8666affa9b
drazen04 <hangtime23@hotmail.it>
2023-01-26 14:25:35 +0100

    xunit: add TestCase and WasRun classes

--- xunit/Main.py
+++ xunit/Main.py
@@ -1,14 +1,4 @@
-class WasRun:
-    def __init__(self, name):
-        self.wasRun = None
-        self.name = name
-    
-    def testMethod(self):
-        self.wasRun = 1
-    
-    def run(self):
-        method = getattr(self, self.name)
-        method()
+from WasRun import WasRun
 
 test = WasRun("testMethod")
 print(test.wasRun)
---
+++ xunit/WasRun.py
@@ -0,0 +1,14 @@
+from TestCase import TestCase
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        self.name = name
+    
+    def testMethod(self):
+        self.wasRun = 1
+    
+    def run(self):
+        method = getattr(self, self.name)
+        method()
